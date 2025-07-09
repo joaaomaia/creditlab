e7458e03c46f2577c0c7aca21dd316df8113559a
João Maia <52588085+joaaomaia@users.noreply.github.com>
2025-07-09 18:24:26 -0300

    Add delay jitter feature and tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pip install -q pandas numpy matplotlib
+pip install -q pandas numpy matplotlib seaborn
 mkdir -p tests/test_results
 
 pytest -vv --tb=long tests > tests/test_results/test_results.txt
